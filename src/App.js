6525db29d428a2e740ee7e1a955f5d4d8e33e05f
PRECIOU5 <91598515+PRECIOU5@users.noreply.github.com>
2022-10-09 23:18:17 +0700

    Tuan 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,163 @@
 
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello wordd</h1>
+    <div className="trello-huuquy">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns"> {/*các công công việc*/}
+        <div className="column"> {/*cột công việc*/}
+          <header>Động Não</header>
+          <ul>
+            <li>
+              <img src="logo512.png"></img>
+              title: Quy
+            </li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+            <li>Thêm cái bạn muốn</li>
+          </ul>
+          <footer>Thêm công việc mới </footer>
+        </div>
+
+        <div className='column'> {/*cột công việc*/}
+          <header>Động Não</header>
+          <ul>
+              <li>
+                <img src="logo512.png"></img>
+                title: Quy
+              </li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+          </ul>
+          <footer>Thêm công việc mới</footer>
+        </div>
+
+        <div className='column'> {/*cột công việc*/}
+          <header>Động Não</header>
+          <ul>
+              <li>
+                <img src="logo512.png"></img>
+                title: Quy
+              </li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+          </ul>
+          <footer>Thêm công việc mới</footer>
+        </div>
+
+        <div className='column'> {/*cột công việc*/}
+          <header>Động Não</header>
+          <ul>
+              <li>
+                <img src="logo512.png"></img>
+                title: Quy
+              </li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+          </ul>
+          <footer>Thêm công việc mới</footer>
+        </div>
+
+        <div className='column'> {/*cột công việc*/}
+          <header>Động Não</header>
+          <ul>
+              <li>
+                <img src="logo512.png"></img>
+                title: Quy
+              </li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+          </ul>
+          <footer>Thêm công việc mới</footer>
+        </div>
+
+        <div className='column'> {/*cột công việc*/}
+          <header>Động Não</header>
+          <ul>
+              <li>
+                <img src="logo512.png"></img>
+                title: Quy
+              </li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+          </ul>
+          <footer>Thêm công việc mới</footer>
+        </div>
+
+        <div className='column'> {/*cột công việc*/}
+          <header>Động Não</header>
+          <ul>
+              <li>
+                <img src="logo512.png"></img>
+                title: Quy
+              </li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+          </ul>
+          <footer>Thêm công việc mới</footer>
+        </div>
+
+        <div className='column'> {/*cột công việc*/}
+          <header>Động Não</header>
+          <ul>
+              <li>
+                <img src="logo512.png"></img>
+                title: Quy
+              </li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+              <li>Thêm cái bạn muốn</li>
+          </ul>
+          <footer>Thêm công việc mới</footer>
+        </div>
+      </div>
     </div>
   );
 }
